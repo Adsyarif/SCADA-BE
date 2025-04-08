d769431102ceb4527c3b99c82d576d9e94e01cc9
adrisa <adsyarif28@gmail.com>
2025-04-09 05:41:49 +0700

    feat:set up project api report

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UserRoleModule } from './user-role/user-role.module';
 import { ConfigModule } from '@nestjs/config';
+import { ReportsModule } from './reports/reports.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    AuthModule, UsersModule, UserRoleModule],
+    AuthModule, UsersModule, UserRoleModule, ReportsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
