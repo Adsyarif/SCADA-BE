7374946cfa76dd316a0b0abdad6a31d325df64ad
adrisa <adsyarif28@gmail.com>
2025-04-12 10:08:19 +0700

    feat: add create report API

---
+++ src/reports/reports.service.ts
@@ -0,0 +1,58 @@
+import { HttpException, Inject, Injectable } from '@nestjs/common';
+import { ValidationService } from '../common/validation.service';
+import {
+  CreateReportRequest,
+  CreateReportResponse,
+} from 'src/model/report.model';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { PrismaService } from 'prisma/prisma.service';
+import { ReportValidation } from './reports.validation';
+
+@Injectable()
+export class ReportService {
+  constructor(
+    private validationService: ValidationService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+    private prismaService: PrismaService,
+  ) {}
+
+  async createReport(
+    request: CreateReportRequest,
+  ): Promise<CreateReportResponse> {
+    this.logger.info(`Register new report: ${JSON.stringify(request)}`);
+
+    const createReportRequest: CreateReportRequest =
+      this.validationService.validate(ReportValidation.CREATE, request);
+
+    const createReport = await this.prismaService.report.create({
+      data: {
+        reportToId: createReportRequest.reportToId,
+        reportFromId: createReportRequest.reportFromId,
+        reportCategoryId: createReportRequest.reportCategoryId,
+        report_description: createReportRequest.reportDescription,
+        updated_by: createReportRequest.updatedBy,
+        report_image: createReportRequest.reportImage,
+      },
+    });
+
+    const checkReport = await this.prismaService.report.findUnique({
+      where: { id: createReport.id },
+      include: {
+        reportTo: true,
+        reportFrom: true,
+      },
+    });
+
+    if (!checkReport) {
+      throw new HttpException('Failed to retrieve newly created report', 500);
+    }
+
+    const response: CreateReportResponse = {
+      reportTo: checkReport.reportTo,
+      reportFrom: checkReport.reportFrom,
+    };
+
+    return response;
+  }
+}
