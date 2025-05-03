6267fe608808b454e552e5c748636254c02f7d66
adrisa <adsyarif28@gmail.com>
2025-05-03 22:32:32 +0700

    feat/add get operator api

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { PrismaService } from 'prisma/prisma.service';
 import { ReportsModule } from './reports/reports.module';
 import { CommonModule } from './common/common.module';
 import { PermissionsModule } from './permissions/permissions.module';
+import { AttendanceModule } from './attendance/attendance.module';
+import { AttendaceController } from './attendace/attendace.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    AuthModule, UsersModule, UserRoleModule, ReportsModule, CommonModule, PermissionsModule],
-  controllers: [AppController],
+    AuthModule, UsersModule, UserRoleModule, ReportsModule, CommonModule, PermissionsModule, AttendanceModule],
+  controllers: [AppController, AttendaceController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
