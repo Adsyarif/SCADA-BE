afe3d7887c7ae65a7270df990c1fe425e4759413
yuliussmayoru <yuliussmayoru@gmail.com>
2025-05-10 18:23:03 +0700

    adding haversine-formula for checkin

--- prisma/migrations/20250508130508_add_radius_to_rtu_configuration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `radius` to the `RtuConfiguration` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "RtuConfiguration" ADD COLUMN     "radius" DOUBLE PRECISION NOT NULL;
